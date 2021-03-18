4955e7f8b7f5bd4daf835fd8e1f951167c5779ff
Samuel Atz <samuel.atz@s3.gt>
2021-03-18 17:51:16 -0600

    Change logo

--- src/containers/Layout/topbar/Topbar.jsx
+++ src/containers/Layout/topbar/Topbar.jsx
@@ -21,7 +21,7 @@ class Topbar extends PureComponent {
               changeMobileSidebarVisibility={changeMobileSidebarVisibility}
               changeSidebarVisibility={changeSidebarVisibility}
             />
-            <Link className="topbar__logo" to="/dashboard_default" />
+            <Link to="/dashboard_default" />
           </div>
           <div className="topbar__right">
             <TopbarProfile />
